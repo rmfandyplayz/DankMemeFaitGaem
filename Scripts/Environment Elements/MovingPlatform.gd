82129cfb8e852d0b7ee5a46081baf74de9da72b7
rmfandyplayz <andyjia2500@gmail.com>
2025-02-13 10:19:12 -0800

    Update 11

--- Scripts/Environment Elements/MovingPlatform.gd
+++ Scripts/Environment Elements/MovingPlatform.gd
@@ -1,4 +1,4 @@
-extends CharacterBody2D
+extends AnimatableBody2D
 
 @export var moveDuration : float # how long does it take for the platform to get to the other destination postion?
 @export var easing : Tween.EaseType
@@ -7,12 +7,24 @@ extends CharacterBody2D
 @export var relativePos1 : Vector2 # the first relative position the platform will move to. the platform will also begin here
 @export var relativePos2 : Vector2 # the second relative position the platform will move to
 
+@export var platformSprite : AnimatedSprite2D
+
+var disablePlatform = false
+
+
 func _ready() -> void:
-	position = relativePos1
+	platformSprite.play()
+	
+	position = Vector2(position.x - relativePos1.x, relativePos1.y)
 	StartPlatformMovement()
 	
 func StartPlatformMovement() -> void:
-	create_tween().set_ease(easing).set_trans(transition).tween_property(self, "position", relativePos2, moveDuration)
-	await get_tree().create_timer(pauseDuration).timeout
-	create_tween().set_ease(easing).set_trans(transition).tween_property(self, "position", relativePos1, moveDuration)
-	await get_tree().create_timer(pauseDuration).timeout
+	while disablePlatform == false:
+		await create_tween().set_ease(easing).set_trans(transition).tween_property(self, "position", relativePos2, moveDuration).finished
+		platformSprite.scale = Vector2(1, 1)
+		await get_tree().create_timer(pauseDuration).timeout
+		await create_tween().set_ease(easing).set_trans(transition).tween_property(self, "position", relativePos1, moveDuration).finished
+		platformSprite.scale = Vector2(-1, 1)
+		await get_tree().create_timer(pauseDuration).timeout
+		
+		
