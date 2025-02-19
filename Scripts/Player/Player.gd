7bda9bd871b5b35d91b5540d2443424996ea151d
rmfandyplayz <andyjia2500@gmail.com>
2025-02-19 10:09:43 -0800

    Update 12

--- Scripts/Player/Player.gd
+++ Scripts/Player/Player.gd
@@ -1,11 +1,8 @@
 extends CharacterBody2D
 
 @export var moveSpeed : float
-@export var acceleration: float
-@export var friction: float  # also how fast we decelerate
-
-@export var burstSpeed : float
-@export var burstMaxDuration : float # in seconds
+@export var acceleration : float
+@export var friction : float  # also how fast we decelerate
 
 @export var playerSprite : AnimatedSprite2D
 @export var minSpriteAnimationSpeed : float = 0.75
@@ -15,16 +12,30 @@ extends CharacterBody2D
 var disableWalkSFX : bool = false
 @export var speedLines : ColorRect
 
+
+@export var burstSpeed : float
+@export var burstMaxDuration : float # in seconds
+@export var burstRechargeSpeed : float # how many seconds per second
+@export var burstRechargeCooldown : float # how long after using burst does it start recharging? (in seconds)
+var burstRechargeCooldownTimer : float
+@export var burstBar : TextureProgressBar
 var burstActive : bool = false
 var burstDirection : Vector2 = Vector2.ZERO
-var burstTimer : float = 0 # current amount 
+var burstTimer : float = 0 # current amount of available burst
 
 func _ready() -> void:
+	#burst bar initial configs
+	burstBar.max_value = burstMaxDuration
+	burstTimer = burstMaxDuration # start full
+	burstBar	.value = burstTimer
+
+	#player sprite animation config
 	playerSprite.animation = "idle"
 	playerSprite.play()
 
 
 func _process(delta: float) -> void:
+	print(burstTimer)
 	# player sprite changes
 	speedLines.visible = velocity != Vector2.ZERO
 	if(velocity == Vector2.ZERO):
@@ -40,13 +51,17 @@ func _process(delta: float) -> void:
 		if disableWalkSFX == false: 
 			walkingSFX.play()
 			disableWalkSFX = true
-		
+
 	speedLines.material.set_shader_parameter("line_density", lerp(0.1, 0.7, clamp(velocity.length() / burstSpeed, 0, 1)))
-	
+
 	# movement and burst
 	if(burstActive == true and velocity != Vector2.ZERO):
 		# burst movement
 		burstTimer -= delta
+
+		# update burst bar
+		burstBar.value = burstTimer 
+		
 		if(burstTimer <= 0 or Input.is_action_pressed("Player_Burst") == false):
 			EndBurst()
 	else:
@@ -58,6 +73,13 @@ func _process(delta: float) -> void:
 			MovePlayer(inputDirection, delta)
 
 	move_and_slide()
+	
+	if (burstActive == false):
+		if burstRechargeCooldownTimer > 0:
+			burstRechargeCooldownTimer -= delta
+		elif burstTimer < burstMaxDuration:
+			burstTimer = min(burstTimer + burstRechargeSpeed * delta, burstMaxDuration)
+			burstBar.value = burstTimer
 
 
 func MovePlayer(inputDirection : Vector2, delta : float):
@@ -73,13 +95,17 @@ func MovePlayer(inputDirection : Vector2, delta : float):
 var burstMusic # the actual audio file/audio stream of the burst music
 var burstMusicTimestamp : float = 0 # so that the music can pick off where it left off
 func StartBurst(direction : Vector2):
+	# abort the burst if there is no available time
+	if(burstTimer <= 0):
+		return
+	
 	disableWalkSFX = true
 	walkingSFX.stop()
 	playerSprite.animation = "boost"
 	playerSprite.speed_scale = 1
 	burstActive = true
 	burstDirection = direction
-	burstTimer = burstMaxDuration
+	
 	velocity = burstDirection * burstSpeed
 	burstMusic = load("res://Media/Audio/Music/gasGasGas.mp3")
 	AudioSystem.PlaySound(load("res://Media/Audio/SFX/sonicBoost.mp3"), "SFX_Master", .1, 1, false)
@@ -90,6 +116,10 @@ func EndBurst():
 	playerSprite.animation = "walk"
 	burstActive = false
 	disableWalkSFX = false
+	
+	#start the cooldown before the bar starts recharging
+	burstRechargeCooldownTimer = burstRechargeCooldown
+	
 	burstMusicTimestamp = AudioSystem.StopAudio(burstMusic, true, 1)
 
 
